51b4cbe033326dcbcec88cc3ee577b3fe9085f03
Sudhakar Tangellapalli <sudhakar.tangellapalli@tagitmobile.com>
2020-10-06 22:34:15 +0530

    Initial commit with Config server

---
+++ src/test/java/com/jgsudhakar/spring/configserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgsudhakar.spring.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
